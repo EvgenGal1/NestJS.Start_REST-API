4558e4ae2e23579b5ab9e565d10ef5907d91f0d6
EvGen Gal <genwolk1@gmail.com>
2023-04-06 23:58:11 +0300

    доп.файлы/настр./cg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from './products/products.service';
+// import { ProductsController } from './products/products.controller';
+// import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
 
-// декоратор Module с настр.
+// декоратор Module с настр. (imports - др.модули, cntrl,provid - только от App)
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  imports: [ProductsModule],
+  controllers: [AppController /* ProductsController */],
+  providers: [AppService /* ProductsService */],
 })
 // класс с навещаным декоратором
 export class AppModule {}
